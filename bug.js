c72606b3eb29d2499896f3814cc676b2e5d482ef
BugHunter-X <askzenai@gmail.com>
2024-12-31 10:02:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// SomeComponent.js
+<div className="bg-gradient-to-r from-blue-500 to-purple-500">
+  {/* ... */}
+</div>
+```
+This code snippet uses Tailwind's gradient utility classes, but there might be cases where the gradient doesn't render correctly, or the colors are unexpected. This can happen due to various reasons such as incorrect class names, conflicting styles, or missing dependencies.
